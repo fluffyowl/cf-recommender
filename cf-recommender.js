25c7880065e9c9cc4812ec399192f5c9b99bd036
fluffyowl <nebukuro09@gmail.com>
2019-06-15 08:17:21 +0900

    Implement JSON fetcher using Codeforces API

---
+++ cf-recommender.js
@@ -0,0 +1,37 @@
+const API_PREFIX = "http://codeforces.com/api/"
+
+function buttonOnClick() {
+    handle = document.handle_form.cf_handle.text;
+    document.location.href = document.location + "?" + handle
+}
+
+function parseIdFromUrlParameters() {
+    return (new URL(document.location)).searchParams.get("cf_handle");
+}
+
+async function getJson(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  }
+
+async function getRating(handle) {
+    const apiUrl = API_PREFIX + "user.info?handles=" + handle
+    const json = await getJson(apiUrl);
+    const user = await json.result[0]; // 最初のユーザを取り出す
+    return user.rating;
+}
+
+async function
+
+window.onload = async function displayContentForId() {
+    const handle = parseIdFromUrlParameters();
+    if (handle == null) return;
+
+    target = document.getElementById("error_text");
+    rating = await getRating(handle);
+
+    target.innerHTML = rating;
+}
+
+
